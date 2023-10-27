818922b5de7eeea48d2f2d055159e68d62158cd9
b3nn3tt <chunterbennett@gmail.com>
2023-10-27 16:46:47 +0300

    Changed case sensitive var's for counters

--- Linux/RHEL_9/functions/general/remediation_output.sh
+++ Linux/RHEL_9/functions/general/remediation_output.sh
@@ -17,14 +17,14 @@ remediation_output()
 {
 	case "$output_code" in
 		101)
-			passed_recommendations=$((passed_recommendations+1))
+			PASSED_RECOMMENDATIONS=$((PASSED_RECOMMENDATIONS+1))
 			echo "*****************************************************************" | tee -a "$SUMMARY_LOG" 2>> "$ERROR_LOG"
 			echo " - $RN - $RNA -" | tee -a "$SUMMARY_LOG" 2>> "$ERROR_LOG"
 			echo " - PASSED - Remediation not required" | tee -a "$SUMMARY_LOG" 2>> "$ERROR_LOG"
 			echo "*****************************************************************" | tee -a "$SUMMARY_LOG" 2>> "$ERROR_LOG"
 			;;
 		102)
-			failed_recommendations=$((failed_recommendations+1))
+			FAILED_RECOMMENDATIONS=$((FAILED_RECOMMENDATIONS+1))
 			echo "*****************************************************************" | tee -a "$SUMMARY_LOG" 2>> "$ERROR_LOG"
 			echo " - $RN - $RNA -" | tee -a "$SUMMARY_LOG" 2>> "$ERROR_LOG"
 			echo " - FAILED - Recommendation failed remediation" | tee -a "$SUMMARY_LOG" 2>> "$ERROR_LOG"
@@ -36,28 +36,28 @@ remediation_output()
 			echo "*****************************************************************" | tee -a "$FAILED_LOG" 2>> "$ERROR_LOG"
 			;;
 		103)
-			remediated_recommendations=$((remediated_recommendations+1))
+			REMEDIATED_RECOMMENDATIONS=$((REMEDIATED_RECOMMENDATIONS+1))
 			echo "*****************************************************************" | tee -a "$SUMMARY_LOG" 2>> "$ERROR_LOG"
 			echo " - $RN - $RNA -" | tee -a "$SUMMARY_LOG" 2>> "$ERROR_LOG"
 			echo " - REMEDIATED - Recommendation successfully remediated" | tee -a "$SUMMARY_LOG" 2>> "$ERROR_LOG"
 			echo "*****************************************************************" | tee -a "$SUMMARY_LOG" 2>> "$ERROR_LOG"
 			;;
 		104)
-			not_applicable_recommendations=$((not_applicable_recommendations+1))
+			NOT_APPLICABLE_RECOMMENDATIONS=$((NOT_APPLICABLE_RECOMMENDATIONS+1))
 			echo "*****************************************************************" | tee -a "$SUMMARY_LOG" 2>> "$ERROR_LOG"
 			echo " - $RN - $RNA -" | tee -a "$SUMMARY_LOG" 2>> "$ERROR_LOG"
 			echo " - N/A - Recommendation is non applicable" | tee -a "$SUMMARY_LOG" 2>> "$ERROR_LOG"
 			echo "*****************************************************************" | tee -a "$SUMMARY_LOG" 2>> "$ERROR_LOG"
 			;;
 		105)
-			excluded_recommendations=$((excluded_recommendations+1))
+			EXCLUDED_RECOMMENDATIONS=$((EXCLUDED_RECOMMENDATIONS+1))
 			echo "*****************************************************************" | tee -a "$SUMMARY_LOG" 2>> "$ERROR_LOG"
 			echo " - $RN - $RNA -" | tee -a "$SUMMARY_LOG" 2>> "$ERROR_LOG"
 			echo " - EXCLUDED - Recommendation on the excluded list" | tee -a "$SUMMARY_LOG" 2>> "$ERROR_LOG"
 			echo "*****************************************************************" | tee -a "$SUMMARY_LOG" 2>> "$ERROR_LOG"
 			;;
 		106)
-			manual_recommendations=$((manual_recommendations+1))
+			MANUAL_RECOMMENDATIONS=$((MANUAL_RECOMMENDATIONS+1))
 			echo "*****************************************************************" | tee -a "$SUMMARY_LOG" 2>> "$ERROR_LOG"
 			echo " - $RN - $RNA -" | tee -a "$SUMMARY_LOG" 2>> "$ERROR_LOG"
 			echo " - MANUAL - Recommendation needs to be remediated manually" | tee -a "$SUMMARY_LOG" 2>> "$ERROR_LOG"
@@ -69,14 +69,14 @@ remediation_output()
 			echo "*****************************************************************" | tee -a "$MANUAL_LOG" 2>> "$ERROR_LOG"
 			;;
 		107)
-			skipped_recommendations=$((skipped_recommendations+1))
+			SKIPPED_RECOMMENDATIONS=$((SKIPPED_RECOMMENDATIONS+1))
 			echo "*****************************************************************" | tee -a "$SUMMARY_LOG" 2>> "$ERROR_LOG"
 			echo " - $RN - $RNA -" | tee -a "$SUMMARY_LOG" 2>> "$ERROR_LOG"
 			echo " - SKIPPED - Recommendation not in selected profile" | tee -a "$SUMMARY_LOG" 2>> "$ERROR_LOG"
 			echo "*****************************************************************" | tee -a "$SUMMARY_LOG" 2>> "$ERROR_LOG"
 			;;
 		201)
-			remediated_recommendations=$((remediated_recommendations+1))
+			REMEDIATED_RECOMMENDATIONS=$((REMEDIATED_RECOMMENDATIONS+1))
 			echo "*****************************************************************" | tee -a "$SUMMARY_LOG" 2>> "$ERROR_LOG"
 			echo " - $RN - $RNA -" | tee -a "$SUMMARY_LOG" 2>> "$ERROR_LOG"
 			echo " - REMEDIATED - Recommendation remediation run" | tee -a "$SUMMARY_LOG" 2>> "$ERROR_LOG"
